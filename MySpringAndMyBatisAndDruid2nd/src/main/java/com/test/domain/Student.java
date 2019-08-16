f158f305fa1a52cd910d577376d6d2b9146c35e6
保登心爱 <lhjer@qq.com>
2019-08-16 11:43:52 +0800

    2nd

---
+++ MySpringAndMyBatisAndDruid2nd/src/main/java/com/test/domain/Student.java
@@ -0,0 +1,13 @@
+package com.test.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Student {
+    private int ID;
+    private String name;
+}
