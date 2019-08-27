d19f617f98e9587375056ff40c1379c9c8e2d8fb
保登心爱 <lhjer@qq.com>
2019-08-27 19:54:30 +0800

    测试Servlet 获得绝对路径,测试转发,多线程安全问题

--- MyServletTest/src/main/java/com/test/servlet/TestServlet.java
+++ MyServletTest/src/main/java/com/test/servlet/TestServlet.java
@@ -1,27 +1,71 @@
 package com.test.servlet;
 
-import javax.servlet.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
-public class TestServlet implements Servlet {
+import static java.lang.Thread.sleep;
 
-    public void init(ServletConfig servletConfig) throws ServletException {
-        System.out.println("初始化");
+
+public class TestServlet extends HttpServlet {
+
+    //    private static Integer i = 100;
+    private ServletConfig servletConfig;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("Service");
+
+
     }
 
-    public ServletConfig getServletConfig() {
-        return null;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.servletConfig = config;
     }
 
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("开始执行业务层");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("接收到了Get请求");
+
+
+        //获取配置信息
+//        ServletContext servletContext = servletConfig.getServletContext();
+//        Enumeration initParameterNames = servletContext.getInitParameterNames();
+//        while (initParameterNames.hasMoreElements()) {
+//            String a = (String) initParameterNames.nextElement();
+//            System.out.println(a+":"+servletContext.getInitParameter(a));
+//        }
+
+        //获取资源绝对路径
+//        String realPath = servletContext.getRealPath("/com/test/servlet/TestServlet.java");
+//        String realPath1 = servletContext.getRealPath("/TestServlet.java");
+//        System.out.println(realPath);
+//        System.out.println(realPath1);
+
+        //测试转发
+//        req.getRequestDispatcher("/TestServletDispatcher").forward(req,resp);
+
+        //测试多线程安全问题
+//        try {
+//            Thread.currentThread().sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(--i);
     }
 
-    public String getServletInfo() {
-        return null;
-    }
-
-    public void destroy() {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        System.out.println("接收到了Post请求");
 
     }
 }
