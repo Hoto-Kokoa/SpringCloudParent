6362dd01ba192ea64f4ffd6ca33263333fcb0f1d
保登心爱 <lhjer@qq.com>
2019-09-25 17:30:04 +0800

    测试使用Ajax

--- demo/src/main/java/com/example/demo/controller/MainActivity.java
+++ demo/src/main/java/com/example/demo/controller/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.demo.controller;
 import com.example.demo.domain.Student;
 import com.example.demo.service.IStudentService;
 import com.example.demo.service.impl.StudentServiceImpl;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -51,5 +55,28 @@ public class MainActivity {
     @RequestMapping("/redis")
     public void testRedis() {
         stringRedisTemplate.opsForValue().set("testSpringBootRedis","123");
+    }
+
+    @RequestMapping("/testAjax")
+    @ResponseBody
+    public List<Student> testAjax() {
+        System.out.println("接受到了请求");
+        List<Student> students = istudentService.findAll();
+        return students;
+
+    }
+
+    @RequestMapping("/testAdmin")
+    @ResponseBody
+    public String testAdmin(String username) {
+        System.out.println("接收到请求");
+        if (username.equals("admin")) {
+            System.out.println("ok");
+            return "ok";
+        }
+        else {
+            System.out.println("no");
+            return "no";
+        }
     }
 }
